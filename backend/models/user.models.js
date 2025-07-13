bf227e757cab578ccb25be8372c123dbf19e1df5
Nishant Kumar Bhadani <134142263+NishantkumarBhadani@users.noreply.github.com>
2025-07-13 18:23:55 +0530

    Created user model

---
+++ backend/models/user.models.js
@@ -0,0 +1,74 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema=new mongoose.Schema({
+
+    fullname:{
+        type:String,
+        required:true,
+        trim:true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    role:{
+        type:String,
+        enum:["user","admin"],
+        default:"user",
+    },
+    refreshToken:{
+        type:String,
+    },
+    votedFor:{
+        type:Types.ObjectId,
+        ref:"Vote",
+    }
+},
+{timestamps:true,});
+
+//Password hashing
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+    this.password=await bcrypt.hash(this.password,10);
+    next();
+})
+//Compare pass
+userSchema.methods.isPassWordCorrect=async function (password) {
+    return await bcrypt.compare(password,this.password);
+}
+userSchema.methods.generateAccessToekn=function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            fullname:this.fullname,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY,
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToekn=function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY,
+        }
+    )
+}
+export const user=mongoose.model("User",userSchema);
